1d34e8cf2f5b0e5b2aad6aceaa0890660955e3f5
Thomas Banks <thomas@tombanks.me>
2022-05-02 21:16:23 +0100

    Replaced github links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,101 +6,101 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: "Websockets",
-	tagline: "A better websocket client",
-	url: "https://tombanks.me",
-	baseUrl: "/",
-	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "warn",
-	favicon: "img/favicon.ico",
-	organizationName: "tombanksme", // Usually your GitHub org/user name.
-	projectName: "websockets", // Usually your repo name.
+    title: "Websockets",
+    tagline: "A better websocket client",
+    url: "https://tombanks.me",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "tombanksme", // Usually your GitHub org/user name.
+    projectName: "websockets", // Usually your repo name.
 
-	presets: [
-		[
-			"classic",
-			/** @type {import('@docusaurus/preset-classic').Options} */
-			({
-				docs: {
-					sidebarPath: require.resolve("./sidebars.js"),
-					// Please change this to your repo.
-					editUrl:
-						"https://github.com/tombanksme/websocket-documentation/tree/main/packages/create-docusaurus/templates/shared/",
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					editUrl:
-						"https://github.com/tombanksme/websocket-documentation/tree/main/packages/create-docusaurus/templates/shared/",
-				},
-				theme: {
-					customCss: require.resolve("./src/css/custom.css"),
-				},
-			}),
-		],
-	],
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    // Please change this to your repo.
+                    editUrl:
+                        "https://github.com/tombanksme/websocket-documentation/tree/main/packages/create-docusaurus/templates/shared/",
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl:
+                        "https://github.com/tombanksme/websocket-documentation/tree/main/packages/create-docusaurus/templates/shared/",
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            }),
+        ],
+    ],
 
-	themeConfig:
-		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-		({
-			navbar: {
-				title: "My Site",
-				logo: {
-					alt: "My Site Logo",
-					src: "img/logo.svg",
-				},
-				items: [
-					{
-						type: "doc",
-						docId: "introduction",
-						position: "left",
-						label: "Tutorial",
-					},
-					{
-						href: "https://github.com/facebook/docusaurus",
-						label: "GitHub",
-						position: "right",
-					},
-				],
-			},
-			footer: {
-				style: "dark",
-				links: [
-					{
-						title: "Docs",
-						items: [
-							{
-								label: "Tutorial",
-								to: "/docs/introduction",
-							},
-						],
-					},
-					{
-						title: "Community",
-						items: [
-							{
-								label: "Twitter",
-								href: "https://twitter.com/tombanksme",
-							},
-						],
-					},
-					{
-						title: "More",
-						items: [
-							{
-								label: "GitHub",
-								href: "https://github.com/tombanksme/websockets",
-							},
-						],
-					},
-				],
-				copyright: `Copyright © ${new Date().getFullYear()} Thomas Banks`,
-			},
-			prism: {
-				theme: lightCodeTheme,
-				darkTheme: darkCodeTheme,
-			},
-		}),
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: "My Site",
+                logo: {
+                    alt: "My Site Logo",
+                    src: "img/logo.svg",
+                },
+                items: [
+                    {
+                        type: "doc",
+                        docId: "introduction",
+                        position: "left",
+                        label: "Tutorial",
+                    },
+                    {
+                        href: "https://github.com/tombanksme/websocket",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                ],
+            },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "Docs",
+                        items: [
+                            {
+                                label: "Tutorial",
+                                to: "/docs/introduction",
+                            },
+                        ],
+                    },
+                    {
+                        title: "Community",
+                        items: [
+                            {
+                                label: "Twitter",
+                                href: "https://twitter.com/tombanksme",
+                            },
+                        ],
+                    },
+                    {
+                        title: "More",
+                        items: [
+                            {
+                                label: "GitHub",
+                                href: "https://github.com/tombanksme/websocket",
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Thomas Banks`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
 };
 
 module.exports = config;
